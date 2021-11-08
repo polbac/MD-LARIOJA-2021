d53e839dba46a8bf5c2706481dc6d121639f78f7
polbac <polbac@gmail.com>
2021-11-08 20:19:13 -0300

    cambio ejercicio

---
+++ backend/c4-m(v)c-json/EJERCITACION.md
@@ -0,0 +1,9 @@
+# Ejercitacion lista/detalle
+
+A partir del siguiente json https://gist.github.com/keeguon/2310008
+hacer la siguiente ejercitacion
+
+1. Generar una app en node
+2. Copiar el json en alguna parte del proyecto para poder cargarlo
+3. Crear una ruta /countries que devuelva el listado de paises
+4. Crear una ruta /countries/:code que devuelva el país búscandolo en el json
---
+++ backend/c4-m(v)c-json/live-coding/app.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const productsJSON = require('./products.json')
+const app = express()
+const PORT = 3000
+/* 
+1) Definir una ruta que sea /products
+2) Definir una ruta '/products/:id' que nos devuelva el objecto que represente al :id
+/products/jenlooper-cactus
+/products/jenlooper-lightshow
+*/
+// definimos rutas
+app.get('/products', (req, res) => {
+    res.send(productsJSON)
+})
+
+app.get('/products/:id', (req, res)=>{
+    const id = req.params.id;
+    const foundProduct = productsJSON.find(function(product) {
+        console.log('product matchea con el id de la url?', product.Id === id)
+        /* tenemos que retornar un booleano si el elemento es el que estamos
+        buscando o no (true, false) */
+        return product.Id === id
+    })
+    
+    if (foundProduct) {
+        // entra si foundProduct es distinto de undefined o null    
+        
+        // respuesta correcta
+        res.status(200).send(foundProduct)
+
+    } else {
+        // el foundProduct es undefined o null
+
+        // error
+        res.status(404).send(`No encontramos el producto ${id}`)
+    }  
+})
+
+app.listen(PORT, () => {
+    console.log(`Aplicación express corriendo en el puerto ${PORT}`)
+})
