4d8e4a2d4492eefeb77053a1ee1bae7cafc96b92
polbac <polbac@gmail.com>
2021-11-25 19:59:33 -0300

    live coding

---
+++ frontend/c10-storage/live-coding/index.js
@@ -0,0 +1,40 @@
+const THEME_LIGHT = 'light'
+const THEME_DARK = 'dark'
+
+let theme = localStorage.getItem('theme') || THEME_LIGHT
+const themeSwitchButton = document.getElementById('switch-theme')
+const body = document.querySelector('body')
+
+applyTheme()
+
+function toggleTheme() {
+    if (theme === THEME_LIGHT) {
+        theme = THEME_DARK
+    } else {
+        theme = THEME_LIGHT
+    }
+
+    // terneario
+    /* theme = theme === THEME_LIGHT ? THEME_DARK : THEME_LIGHT */
+
+    // escribir en el localStorage el theme
+    localStorage.setItem('theme', theme)
+    
+    applyTheme()
+}
+
+function applyTheme() {
+    if (theme === THEME_DARK) {
+        body.classList.add('dark')
+        themeSwitchButton.innerText = 'Change to light mode'
+    } else {
+        body.classList.remove('dark')
+        themeSwitchButton.innerText = 'Change to dark mode'
+    }
+    
+}
+
+themeSwitchButton.addEventListener('click', toggleTheme)
+
+
+const miJSON = '[{"name":"Guitar Fender Stratocaster",price:1000},{"name":"Guitar Fender Telecaster",price:1500}]'
