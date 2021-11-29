d72411aced3d26bfd8cb535bf4f03c1815a690de
polbac <polbac@gmail.com>
2021-11-29 20:21:23 -0300

    .

--- backend/c8-hash/ejercicio/controllers/userController.js
+++ backend/c8-hash/ejercicio/controllers/userController.js
@@ -1,43 +1,33 @@
 const fs = require('fs')
-const { UserLoginSchema, UserRegisterSchema } = require('../schemas/user')
 const usersFile = require('./users.json')
 
 function register(req, res) {
     // TODO: guardear el password hasheado
+    // [POST] http://localhost:3000/user/register
     const { name, email, password } = req.body
 
-    const validUser = userSchema.validate({ name, email, password })
+    const newUser = {
+        name, 
+        email,
+        password,
+    }
 
-    if (validUser.error) {
-        res.send(validUser.error.details)
-    } else {
-        const newUser = {
-            name, 
-            email,
-            password,
-        }
+    usersFile.push(newUser)
 
-        usersFile.push(newUser)
-
-        fs.writeFileSync(__dirname + '/users.json', JSON.stringify(usersFile))
-        res.send(newUser)
-    }   
+    fs.writeFileSync(__dirname + '/users.json', JSON.stringify(usersFile))
+    res.send(newUser)
+    
 }
 
 function login(req, res) {
     const { email, password } = req.body
+    // [POST] http://localhost:3000/user/login
+    
+    /* TODO: guardar en esta variable si encuentra el usuario en el archivo json
+    a partir del email y el password */
+    const userFound = false
 
-    const validUser = userSchema.validate({ email, password })
-
-    if (validUser.error) {
-        res.send(validUser.error.details)
-    } else {
-        /* TODO: guardar en esta variable si encuentra el usuario en el archivo json
-        a partir del email y el password */
-        const userFound = false
-
-        res.send(userFound)
-    }
+    res.send(userFound)
     
     
 }
